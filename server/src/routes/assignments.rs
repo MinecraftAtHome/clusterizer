ef4f2467e02d465672182ec61b04e452b01eb13e
Chen Steenvoorden <dutchen18@gmail.com>
2025-05-07 23:57:22 +0200

    Move /assignments/fetch to /tasks/fetch, it returns tasks now

--- server/src/routes/assignments.rs
+++ server/src/routes/assignments.rs
@@ -4,7 +4,7 @@ use axum::{
 };
 use clusterizer_common::types::Assignment;
 
-use crate::{auth::Auth, result::ApiResult, state::AppState};
+use crate::{result::ApiResult, state::AppState};
 
 pub async fn get_all(State(state): State<AppState>) -> ApiResult<Vec<Assignment>> {
     Ok(Json(
@@ -20,54 +20,4 @@ pub async fn get_one(State(state): State<AppState>, Path(id): Path<i64>) -> ApiR
             .fetch_one(&state.pool)
             .await?,
     ))
-}
-
-pub async fn fetch(
-    State(state): State<AppState>,
-    Auth(user_id): Auth,
-) -> ApiResult<Vec<Assignment>> {
-    let _guard = state.fetch_mutex.lock().await;
-
-    let record = sqlx::query!(
-        "
-        SELECT
-            t.id
-        FROM
-            tasks t
-            JOIN projects p
-                ON t.project_id = p.id
-                AND p.active
-            LEFT JOIN assignments a
-                ON a.task_id = t.id
-                AND NOT a.canceled
-        WHERE
-            a.id IS NULL
-        "
-    )
-    .fetch_optional(&state.pool)
-    .await?;
-
-    if let Some(record) = record {
-        let assignment = sqlx::query_as!(
-            Assignment,
-            "
-            INSERT INTO assignments (
-                task_id,
-                user_id
-            ) VALUES (
-                $1,
-                $2
-            )
-            RETURNING *
-            ",
-            record.id,
-            user_id
-        )
-        .fetch_one(&state.pool)
-        .await?;
-
-        Ok(Json(vec![assignment]))
-    } else {
-        Ok(Json(vec![]))
-    }
 }
--- server/src/routes/tasks.rs
+++ server/src/routes/tasks.rs
@@ -4,7 +4,7 @@ use axum::{
 };
 use clusterizer_common::types::Task;
 
-use crate::{result::ApiResult, state::AppState};
+use crate::{auth::Auth, result::ApiResult, state::AppState};
 
 pub async fn get_all(State(state): State<AppState>) -> ApiResult<Vec<Task>> {
     Ok(Json(
@@ -20,4 +20,50 @@ pub async fn get_one(State(state): State<AppState>, Path(id): Path<i64>) -> ApiR
             .fetch_one(&state.pool)
             .await?,
     ))
+}
+
+pub async fn fetch(State(state): State<AppState>, Auth(user_id): Auth) -> ApiResult<Vec<Task>> {
+    let _guard = state.fetch_mutex.lock().await;
+
+    let task = sqlx::query_as!(
+        Task,
+        "
+        SELECT
+            t.*
+        FROM
+            tasks t
+            JOIN projects p
+                ON t.project_id = p.id
+                AND p.active
+            LEFT JOIN assignments a
+                ON a.task_id = t.id
+                AND NOT a.canceled
+        WHERE
+            a.id IS NULL
+        "
+    )
+    .fetch_optional(&state.pool)
+    .await?;
+
+    if let Some(task) = task {
+        sqlx::query!(
+            "
+            INSERT INTO assignments (
+                task_id,
+                user_id
+            ) VALUES (
+                $1,
+                $2
+            )
+            ",
+            task.id,
+            user_id
+        )
+        .execute(&state.pool)
+        .await?;
+
+        Ok(Json(vec![task]))
+    } else {
+        Ok(Json(vec![]))
+    }
 }
