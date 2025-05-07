ec57a863c4b8b2fe8f20385aa1ac8d4392f627ed
BoySanic <boysonicrevived@gmail.com>
2025-05-07 15:59:56 -0700

    Put into struct

--- api/src/requests.rs
+++ api/src/requests.rs
@@ -4,72 +4,81 @@ use reqwest::Error;
 use serde::de::DeserializeOwned;
 use serde::{Deserialize, Serialize};
 
-pub async fn get<T: DeserializeOwned>(full: &str, api_key: &str) -> Result<T, Error> {
-    let client = reqwest::Client::new();
-    let res = client
-        .get(full)
-        .header("Authorization", format!("Bearer {}", api_key))
-        .send()
+struct Client {
+    client: reqwest::Client,
+    url: String,
+    api_key: Option<String>,
+}
+
+impl Client{
+
+    pub async fn get<T: DeserializeOwned>(&self, uri: &str) -> Result<T, Error> {
+        let full = format!("{}{}", self.url, uri);
+        let client = reqwest::Client::new();
+        let mut req = client
+            .get(full);
+        if let Some(ref x) = self.api_key{
+            req = req.header("Authorization", format!("Bearer {}", x));
+        }
+        let res = req.send()
         .await?
         .json::<T>()
         .await?;
-    Ok(res)
-}
+        Ok(res)
+    }
 
-pub async fn post<T: Serialize, R: DeserializeOwned>(
-    full: &str,
-    api_key: &str,
-    data: &T,
-) -> Result<R, Error> {
-    let client = reqwest::Client::new();
-    let res = client
-        .post(full)
-        .json(data)
-        .header("Content-Type", "application/json")
-        .header("Authorization", format!("Bearer {}", api_key))
-        .send()
-        .await?
-        .json::<R>()
-        .await?;
-    Ok(res)
-}
+    pub async fn post<T: Serialize, R: DeserializeOwned>(
+        &self,
+        uri: &str,
+        data: &T,
+    ) -> Result<R, Error> {
+        let full = format!("{}{}", self.url, uri);
+        let client = reqwest::Client::new();
+        let mut req = client
+            .post(full)
+            .json(data)
+            .header("Content-Type", "application/json");
+        if let Some(ref x) = self.api_key{
+            req = req.header("Authorization", format!("Bearer {}", x));
+        }
+        let res = req.send()
+            .await?
+            .json::<R>()
+            .await?;
+        Ok(res)
+    }
 
-//GET requests
-pub async fn get_task(base_url: &str, api_key: &str, task_id: i64) -> Result<Task, Error> {
-    let full = format!("{}/tasks/{}", base_url.trim_end_matches('/'), task_id);
-    Ok(get(&full, api_key).await?)
-}
+    //GET requests
+    pub async fn get_task(&self, task_id: i64) -> Result<Task, Error> {
+        let uri = format!("/tasks/{}", task_id);
+        Ok(self.get(&uri).await?)
+    }
 
-pub async fn get_project(base_url: &str, api_key: &str, project_id: i64) -> Result<Project, Error> {
-    let full = format!("{}/projects/{}", base_url.trim_end_matches('/'), project_id);
-    Ok(get(&full, api_key).await?)
-}
+    pub async fn get_project(&self, project_id: i64) -> Result<Project, Error> {
+        let uri = format!("/projects/{}", project_id);
+        Ok(self.get(&uri).await?)
+    }
 
-pub async fn get_project_version(
-    base_url: &str,
-    api_key: &str,
-    project_id: i64,
-) -> Result<Vec<ProjectVersion>, Error> {
-    let full = format!(
-        "{}/projects/{}/project_versions",
-        base_url.trim_end_matches('/'),
-        project_id
-    );
-    Ok(get(&full, api_key).await?)
-}
+    pub async fn get_project_version(&self, project_id: i64,) -> Result<Vec<ProjectVersion>, Error> {
+        let uri = format!(
+            "/projects/{}/project_versions",
+            project_id
+        );
+        Ok(self.get(&uri).await?)
+    }
 
-pub async fn fetch_assignments(base_url: &str, api_key: &str) -> Result<Vec<Assignment>, Error> {
-    let full = format!("{}/assignments/fetch", base_url.trim_end_matches('/'));
-    Ok(get(&full, api_key).await?)
-}
+    pub async fn fetch_assignments(&self,) -> Result<Vec<Assignment>, Error> {
+        let uri = "/assignments/fetch";
+        Ok(self.get(&uri).await?)
+    }
 
-//POST requests
-pub async fn submit_result(
-    task_id: i64,
-    base_url: &str,
-    api_key: &str,
-    result_data: &SubmitRequest,
-) -> Result<SubmitResponse, Error> {
-    let full = format!("{}/results/submit", base_url.trim_end_matches('/'));
-    Ok(post(&full.to_string(), api_key, result_data).await?)
+    //POST requests
+    pub async fn submit_result(
+        &self,
+        task_id: i64,
+        result_data: &SubmitRequest,
+    ) -> Result<SubmitResponse, Error> {
+        let uri = "/results/submit";
+        Ok(self.post(&uri, result_data).await?)
+    }
 }
