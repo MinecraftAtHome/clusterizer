51ed983e2bdac20c534e37188b27e09e6e7f37b5
BoySanic <boysonicrevived@gmail.com>
2025-05-12 07:55:23 -0700

    Implement assignments_remaining (#22)

--- server/src/state.rs
+++ server/src/state.rs
@@ -1,11 +1,6 @@
-use std::sync::Arc;
-
 use sqlx::PgPool;
-use tokio::sync::Mutex;
-
 #[derive(Clone)]
 pub struct AppState {
     pub pool: PgPool,
     pub secret: Vec<u8>,
-    pub fetch_mutex: Arc<Mutex<()>>,
 }
