a5a3d23353e62ba07f42b717383fc136000e9c17
Chen Steenvoorden <dutchen18@gmail.com>
2025-05-16 15:38:39 +0200

    Threading

--- client/src/client.rs
+++ client/src/client.rs
@@ -1,29 +1,42 @@
 use std::{
+    collections::{HashMap, VecDeque},
     env,
     ffi::OsString,
-    fs::{self, File},
+    fs::{self},
+    io::Cursor,
     iter::{self, Empty},
-    thread,
+    process::{Output, Stdio},
+    sync::Arc,
     time::Duration,
 };
 
 use clusterizer_api::client::ApiClient;
 use clusterizer_common::{
+    id::Id,
     requests::{FetchTasksRequest, SubmitResultRequest},
-    types::{ProjectVersion, Task},
+    types::{Project, ProjectVersion, Task},
 };
-use log::{debug, error, info};
-use tokio::process::Command;
+use log::{debug, info, warn};
+use tokio::{io::AsyncWriteExt, process::Command, task::JoinSet, time};
 use zip::ZipArchive;
 
-use crate::{
-    args::RunArgs,
-    result::{ClientError, ClientResult},
-};
+use crate::{args::RunArgs, result::ClientResult};
 
 pub struct ClusterizerClient {
     client: ApiClient,
     args: RunArgs,
+}
+
+struct TaskInfo {
+    task: Task,
+    project: Project,
+    project_version: ProjectVersion,
+}
+
+enum Return {
+    FetchTasks(Vec<TaskInfo>),
+    ExecuteTask(Id<Task>, Output),
+    SubmitResult,
 }
 
 impl ClusterizerClient {
@@ -31,57 +44,143 @@ impl ClusterizerClient {
         ClusterizerClient { client, args }
     }
 
-    pub async fn run(&self) -> ClientResult<()> {
+    pub async fn run(self: Arc<Self>) -> ClientResult<()> {
         fs::create_dir_all(&self.args.cache_dir)?;
 
-        loop {
-            // TODO: cache project versions instead of fetching them each time. the cache can also
-            // be used in execute_task when trying to find the right binary to use for a specific
-            // task.
+        let mut set = JoinSet::new();
+        let mut tasks = VecDeque::new();
+        let mut fetching_tasks = true;
+        let mut used_threads = 0;
 
-            let mut project_ids: Vec<_> = self
+        set.spawn(Arc::clone(&self).fetch_tasks());
+
+        while let Some(ret) = set.join_next().await {
+            match ret?? {
+                Return::FetchTasks(new_tasks) => {
+                    fetching_tasks = false;
+                    tasks.extend(new_tasks);
+                }
+                Return::ExecuteTask(task_id, output) => {
+                    used_threads -= 1;
+                    set.spawn(Arc::clone(&self).submit_result(task_id, output));
+                }
+                Return::SubmitResult => {}
+            }
+
+            let mut out_of_tasks = false;
+
+            while used_threads < self.args.threads {
+                if let Some(task) = tasks.pop_front() {
+                    used_threads += 1;
+                    set.spawn(Arc::clone(&self).execute_task(task));
+                } else {
+                    out_of_tasks = true;
+
+                    break;
+                }
+            }
+
+            if !fetching_tasks && (out_of_tasks || tasks.len() < self.args.queue) {
+                fetching_tasks = true;
+                set.spawn(Arc::clone(&self).fetch_tasks());
+            }
+        }
+
+        Ok(())
+    }
+
+    async fn fetch_tasks(self: Arc<Self>) -> ClientResult<Return> {
+        let tasks = loop {
+            let mut projects: HashMap<_, _> = self
+                .client
+                .get_all::<Project>()
+                .await?
+                .into_iter()
+                .filter(|project| project.disabled_at.is_none())
+                .map(|project| (project.id, project))
+                .collect();
+
+            let projects: HashMap<_, _> = self
                 .client
                 .get_all_by::<ProjectVersion, _>(self.args.platform_id)
                 .await?
                 .into_iter()
                 .filter(|project_version| project_version.disabled_at.is_none())
-                .map(|project_version| project_version.project_id)
+                .filter_map(|project_version| {
+                    projects
+                        .remove(&project_version.project_id)
+                        .map(|project| (project.id, (project, project_version)))
+                })
                 .collect();
 
-            project_ids.sort();
-            project_ids.dedup();
-
-            let tasks = self
+            let tasks: Vec<_> = self
                 .client
-                .fetch_tasks(&FetchTasksRequest { project_ids })
-                .await?;
+                .fetch_tasks(&FetchTasksRequest {
+                    project_ids: projects.keys().copied().collect(),
+                })
+                .await?
+                .into_iter()
+                .filter_map(|task| {
+                    let info = projects
+                        .get(&task.project_id)
+                        .map(|(project, project_version)| TaskInfo {
+                            task,
+                            project: project.clone(),
+                            project_version: project_version.clone(),
+                        });
 
-            if tasks.is_empty() {
-                info!("No tasks found. Sleeping before attempting again.");
-                thread::sleep(Duration::from_millis(15000));
-            } else {
-                for task in tasks {
-                    if let Err(err) = self.execute_task(&task).await {
-                        error!("Error: {err}.");
+                    if info.is_none() {
+                        warn!("Unwanted task received from server.");
                     }
-                }
+
+                    info
+                })
+                .collect();
+
+            if !tasks.is_empty() {
+                break tasks;
+            }
+
+            info!("No tasks found. Sleeping before attempting again.");
+            time::sleep(Duration::from_millis(15000)).await;
+        };
+
+        for TaskInfo {
+            project_version, ..
+        } in &tasks
+        {
+            let project_version_dir = self.args.cache_dir.join(project_version.id.to_string());
+
+            if project_version_dir.exists() {
+                debug!("Archive {} was cached.", project_version_dir.display());
+            } else {
+                debug!("Archive {} is not cached.", project_version_dir.display());
+
+                let bytes = reqwest::get(&project_version.archive_url)
+                    .await?
+                    .error_for_status()?
+                    .bytes()
+                    .await?;
+
+                let extract_dir = tempfile::tempdir_in(&self.args.cache_dir)?;
+
+                ZipArchive::new(Cursor::new(bytes))?.extract(&extract_dir)?;
+                fs::rename(&extract_dir, project_version_dir)?;
             }
         }
+
+        Ok(Return::FetchTasks(tasks))
     }
 
-    async fn execute_task(&self, task: &Task) -> ClientResult<()> {
-        let project = self.client.get_one(task.project_id).await?;
-        let project_version = self
-            .client
-            .get_all_by::<ProjectVersion, _>(task.project_id)
-            .await?
-            .into_iter()
-            .filter(|project_version| project_version.disabled_at.is_none())
-            .find(|project_version| project_version.platform_id == self.args.platform_id)
-            .ok_or(ClientError::ProjectVersionNotFound)?;
-
+    async fn execute_task(
+        self: Arc<Self>,
+        TaskInfo {
+            task,
+            project,
+            project_version,
+        }: TaskInfo,
+    ) -> ClientResult<Return> {
         let slot_dir = tempfile::tempdir()?;
-        let slot_path = slot_dir.path();
 
         info!("Task id: {}, stdin: {}", task.id, task.stdin);
         info!("Project id: {}, name: {}", project.id, project.name);
@@ -89,50 +188,49 @@ impl ClusterizerClient {
             "Project version id: {}, archive url: {}",
             project_version.id, project_version.archive_url
         );
-        debug!("Slot path: {}", slot_path.display());
+        debug!("Slot dir: {}", slot_dir.path().display());
 
-        fs::create_dir_all(slot_path)?;
-
-        let archive_cache_path = &self
+        let program = self
             .args
             .cache_dir
-            .join(project_version.id.to_string() + ".zip");
-
-        if archive_cache_path.is_file() {
-            debug!("Archive {} was cached.", archive_cache_path.display());
-        } else {
-            debug!("Archive {} is not cached.", archive_cache_path.display());
-
-            let bytes = reqwest::get(project_version.archive_url)
-                .await?
-                .error_for_status()?
-                .bytes()
-                .await?;
-
-            fs::write(archive_cache_path, &bytes)?;
-        }
-
-        ZipArchive::new(File::open(archive_cache_path)?)?.extract(slot_path)?;
-
-        let program = slot_path
+            .join(project_version.id.to_string())
             .join(format!("main{}", env::consts::EXE_SUFFIX))
             .canonicalize()?;
+
         let args: Empty<OsString> = iter::empty();
 
-        let output = Command::new(program)
+        let mut child = Command::new(program)
             .args(args)
-            .current_dir(slot_path)
-            .output()
-            .await?;
+            .current_dir(&slot_dir)
+            .stdin(Stdio::piped())
+            .stdout(Stdio::piped())
+            .stderr(Stdio::piped())
+            .spawn()?;
 
-        let result = SubmitResultRequest {
-            stdout: String::from_utf8_lossy(&output.stdout).to_string(),
-            stderr: String::from_utf8_lossy(&output.stderr).to_string(),
+        let mut stdin = child.stdin.take().unwrap();
+
+        tokio::spawn(async move {
+            stdin.write_all(task.stdin.as_bytes()).await.unwrap();
+        });
+
+        let output = child.wait_with_output().await?;
+
+        Ok(Return::ExecuteTask(task.id, output))
+    }
+
+    async fn submit_result(
+        self: Arc<Self>,
+        task_id: Id<Task>,
+        output: Output,
+    ) -> ClientResult<Return> {
+        let request = SubmitResultRequest {
+            stdout: String::from_utf8_lossy(&output.stdout).into_owned(),
+            stderr: String::from_utf8_lossy(&output.stderr).into_owned(),
             exit_code: output.status.code(),
         };
 
-        self.client.submit_result(task.id, &result).await?;
+        self.client.submit_result(task_id, &request).await?;
 
-        Ok(())
+        Ok(Return::SubmitResult)
     }
 }
--- client/src/result.rs
+++ client/src/result.rs
@@ -2,22 +2,22 @@ use std::{error::Error, io};
 
 use clusterizer_api::result::ApiError;
 use thiserror::Error;
+use tokio::task::JoinError;
 use zip::result::ZipError;
 
 #[derive(Debug, Error)]
 #[error(transparent)]
 pub enum ClientError {
-    Specific(Box<dyn Error>),
+    Specific(Box<dyn Error + Sync + Send>),
     Reqwest(#[from] reqwest::Error),
     Zip(#[from] ZipError),
     Io(#[from] io::Error),
-    #[error("project version not found")]
-    ProjectVersionNotFound,
+    Join(#[from] JoinError),
 }
 
 pub type ClientResult<T> = Result<T, ClientError>;
 
-impl<E: Error + 'static> From<ApiError<E>> for ClientError {
+impl<E: Error + Sync + Send + 'static> From<ApiError<E>> for ClientError {
     fn from(err: ApiError<E>) -> Self {
         match err {
             ApiError::Specific(err) => Self::Specific(Box::new(err)),
