4cbd139ece4450c29d4d9e41b78a968d3c18670c
Chen Steenvoorden <dutchen18@gmail.com>
2025-05-23 01:14:46 +0200

    Batch fetching tasks

--- common/src/requests/fetch_tasks_request.rs
+++ common/src/requests/fetch_tasks_request.rs
@@ -5,4 +5,5 @@ use crate::{records::Project, types::Id};
 #[derive(Clone, Hash, Debug, Serialize, Deserialize)]
 pub struct FetchTasksRequest {
     pub project_ids: Vec<Id<Project>>,
+    pub limit: usize,
 }
