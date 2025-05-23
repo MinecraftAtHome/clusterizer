af48123138479e20f129b470f5c1189f7511d4d3
BoySanic <boysonicrevived@gmail.com>
2025-05-23 10:41:22 -0700

    Fix bug in validate_fetch since we're not using a vec of project_ids anymore

--- server/src/routes/validate_fetch.rs
+++ server/src/routes/validate_fetch.rs
@@ -5,24 +5,24 @@ use axum::{
 use clusterizer_common::{
     errors::ValidateFetchError,
     records::{Project, Task},
-    requests::ValidateFetchRequest,
     types::Id,
 };
 
 use crate::{
-    result::{AppResult, ResultExt},
+    result::{AppError, AppResult, ResultExt},
     state::AppState,
 };
 
 pub async fn validate_fetch(
     State(state): State<AppState>,
     Path(project_id): Path<Id<Project>>,
-    Json(request): Json<ValidateFetchRequest>,
 ) -> AppResult<Json<Vec<Task>>, ValidateFetchError> {
-    sqlx::query_scalar_unchecked!(
+    let mut tx = state.pool.begin().await?;
+    let project_result = sqlx::query_as_unchecked!(
+        Project,
         r#"
         SELECT
-            count(*) as "count!"
+            *
         FROM
             projects
         WHERE
@@ -30,34 +30,40 @@ pub async fn validate_fetch(
         "#,
         project_id
     )
-    .fetch_one(&state.pool)
-    .await
-    .map_not_found(ValidateFetchError::InvalidProject)?;
-
-    let task = sqlx::query_as_unchecked!(
-        Task,
-        r#"
-        SELECT
-            t.*
-        FROM
-            tasks t
-            JOIN assignments a ON
-                a.task_id = t.id
-        WHERE
-            a.state not in ('canceled', 'expired')
-        GROUP BY
-            t.id
-        HAVING
-            t.project_id = ANY($1)
-            AND (
-                count(a.id) >= t.assignments_needed
-                OR t.canonical_result_id IS NOT NULL
-                )
-        "#,
-        request.project_ids,
-    )
-    .fetch_all(&state.pool)
+    .fetch_optional(&mut *tx)
     .await?;
 
-    Ok(Json(task.into_iter().collect()))
+    match project_result{
+        Some(project) => {
+             let task = sqlx::query_as_unchecked!(
+                    Task,
+                    r#"
+                    SELECT
+                        t.*
+                    FROM
+                        tasks t
+                        JOIN assignments a ON
+                            a.task_id = t.id
+                    WHERE
+                        a.state not in ('canceled', 'expired')
+                    GROUP BY
+                        t.id
+                    HAVING
+                        t.project_id = $1
+                        AND (
+                            count(a.id) >= t.assignments_needed
+                            OR t.canonical_result_id IS NOT NULL
+                            )
+                    "#,
+                    project.id
+                )
+                .fetch_all(&state.pool)
+                .await?;
+                tx.commit().await?;
+                Ok(Json(task.into_iter().collect()))
+        },
+        None => Err(AppError::Specific(ValidateFetchError::InvalidProject))
+    }
+
+   
 }
