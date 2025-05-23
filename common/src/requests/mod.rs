1c5f22e3d637e641e2bfa4f29fe65125316e0674
BoySanic <boysonicrevived@gmail.com>
2025-05-23 10:41:22 -0700

    Fix bug in validate_fetch since we're not using a vec of project_ids anymore

--- common/src/requests/mod.rs
+++ common/src/requests/mod.rs
@@ -1,11 +1,9 @@
 pub mod fetch_tasks_request;
 pub mod register_request;
 pub mod submit_result_request;
-pub mod validate_fetch_request;
 pub mod validate_submit_request;
 
 pub use fetch_tasks_request::FetchTasksRequest;
 pub use register_request::RegisterRequest;
 pub use submit_result_request::SubmitResultRequest;
-pub use validate_fetch_request::ValidateFetchRequest;
 pub use validate_submit_request::ValidateSubmitRequest;
--- common/src/requests/validate_fetch_request.rs
+++
@@ -1,8 +0,0 @@
-use serde::{Deserialize, Serialize};
-
-use crate::{records::Project, types::Id};
-
-#[derive(Clone, Hash, Debug, Serialize, Deserialize)]
-pub struct ValidateFetchRequest {
-    pub project_ids: Vec<Id<Project>>,
-}
