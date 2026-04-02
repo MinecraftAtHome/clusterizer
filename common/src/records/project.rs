96a03b5879700d29c58ca1f26957404f8200f5c9
Chen Steenvoorden <dutchen18@gmail.com>
2026-04-02 22:29:56 +0200

    Add projects created_by_user_id and restrict validation routes

--- common/src/records/project.rs
+++ common/src/records/project.rs
@@ -1,12 +1,13 @@
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 
-use crate::types::Id;
+use crate::{records::User, types::Id};
 
 #[derive(Clone, Hash, Debug, Serialize, Deserialize)]
 pub struct Project {
     pub id: Id<Project>,
     pub created_at: DateTime<Utc>,
+    pub created_by_user_id: Id<User>,
     pub disabled_at: Option<DateTime<Utc>>,
     pub name: String,
 }
@@ -14,10 +15,16 @@ pub struct Project {
 #[non_exhaustive]
 #[derive(Clone, Hash, Debug, Default, Serialize, Deserialize)]
 pub struct ProjectFilter {
+    pub created_by_user_id: Option<Id<User>>,
     pub disabled: Option<bool>,
 }
 
 impl ProjectFilter {
+    pub fn created_by_user_id(mut self, created_by_user_id: Id<User>) -> Self {
+        self.created_by_user_id = Some(created_by_user_id);
+        self
+    }
+
     pub fn disabled(mut self, disabled: bool) -> Self {
         self.disabled = Some(disabled);
         self
